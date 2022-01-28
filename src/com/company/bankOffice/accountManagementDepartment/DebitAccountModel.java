8e983d0c82e42da98a47c159e65c954893a93ca3
Shakhnoza <student20102@gimail.com>
2022-01-28 22:58:45 +0700

    List of bank clients, list of client's accounts added, code refactored

--- src/com/company/bankOffice/accountManagementDepartment/DebitAccountModel.java
+++ src/com/company/bankOffice/accountManagementDepartment/DebitAccountModel.java
@@ -5,11 +5,14 @@ import com.company.Client;
 import com.company.bankOffice.BankOffice;
 import com.company.service.GenerateAccountNumber;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DebitAccountModel extends  DebitAccount{
     private String nameAccount;
     private String accountNumber;
     private int moneyInAccount;
-    private String openingDate;
+    private Date openingDate;
     private int creditTerm;
 
     private GenerateAccountNumber generateAccountNumber = new GenerateAccountNumber();
@@ -17,18 +20,21 @@ public class DebitAccountModel extends  DebitAccount{
 
     @Override
     void openDebitAccount(Client client, BankOffice bankOffice) {
-        this.nameAccount = nameAccount;
+        this.nameAccount = "Дебетовый счет";
         this.accountNumber = generateAccountNumber.accountNumber();
         this.moneyInAccount = moneyInAccount;
-        this.openingDate = openingDate;
+        this.openingDate = new Date();
         this.creditTerm = 24;
 
+        bankOffice.getClientBankList().put(client.getPassport(),client.getName());
         client.getDebitAccount().put(this.accountNumber, this.nameAccount);
+        client.getListOfAccounts().put(this.accountNumber, this.nameAccount);
 
+        SimpleDateFormat dateFormat = new SimpleDateFormat();
         System.out.println("Для "+ client.getName() + " создан " + nameAccount  +
                 "\n________________ИНФОРМАЦИЯ__________________" +
                 "\nномер счёта: " + accountNumber +
-                "\nдата открытия: " + openingDate +
+                "\nдата открытия: " + dateFormat.format(openingDate) +
                 "\nсроком: "+ creditTerm +" мес."+
                 "\nбаланс счёта: "+ moneyInAccount+
                 "\n____________________________________________");
--- src/com/company/bankOffice/creditDepartment/CreditModel.java
+++ src/com/company/bankOffice/creditDepartment/CreditModel.java
@@ -6,6 +6,7 @@ import com.company.service.GenerateAccountNumber;
 import com.company.bankOffice.BankOffice;
 import com.company.service.ClientService;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
@@ -17,11 +18,16 @@ public class CreditModel extends Credits{
     private Date openingDate;
     private int creditTerm;
     private double paymentMonth;
+    private String cardAccountNumber;
+    private double cardPtc;
+    private Date cardOpeningDate;
+    private String cardCreditName;
+    private int cardAmount;
 
     GenerateAccountNumber generateAccountNumber = new GenerateAccountNumber();
     CreditCalculationPayment payment = new CreditCalculationPayment();
     ClientService clientService  = new ClientService();
-    Date date = new Date();
+    //Date date = new Date();
 
     public void openCredit(String creditName, Client client, int sum, BankOffice bankOffice, double ptc, int creditTerm ) {
 
@@ -29,23 +35,49 @@ public class CreditModel extends Credits{
         this.accountNumber = generateAccountNumber.accountNumber();
         this.amount = sum;
         this.ptc = ptc;
-        this.openingDate = date;
+        this.openingDate = new Date();
         this.creditTerm = creditTerm;
         this.paymentMonth = payment.calc(this.amount,this.ptc,this.creditTerm);
 
         clientService.takeCash(amount,client);
 
         bankOffice.getCreditClientList().put(this.accountNumber,client.getName());
+        bankOffice.getClientBankList().put(client.getPassport(),client.getName());
         client.getCreditListClient().put(this.accountNumber, this.creditName);
+        client.getListOfAccounts().put(this.accountNumber, this.creditName);
 
+        SimpleDateFormat dateFormat = new SimpleDateFormat();
         System.out.println(client.getName() + " вам одобрен " + this.creditName +
                 "\n________________ИНФОРМАЦИЯ__________________" +
                 "\nномер счёта: " + accountNumber +
                 "\nсрок кредита: " + this.creditTerm + " мес." +
-                "\nдата открытия: " + openingDate +
+                "\nдата открытия: " + dateFormat.format(openingDate) +
                 "\nпроцентная ставка: " + this.ptc +
                 "\nсумма займа: " + amount +
                 "\nсумма ежемесячного платежа: " + String.format("%.2f", paymentMonth) + " руб." +
                 "\n____________________________________________");
     }
+
+    public void openCreditCard(String cardCreditName, Client client, int sum, BankOffice bankOffice, double ptc){
+        this.cardAccountNumber = generateAccountNumber.accountNumber();
+        this.cardOpeningDate = new Date();
+        this.cardPtc = ptc;
+        this.cardCreditName = cardCreditName;
+        this.cardAmount = sum;
+
+        bankOffice.getCreditClientList().put(this.cardAccountNumber,client.getName());
+        bankOffice.getClientBankList().put(client.getPassport(),client.getName());
+        client.getCreditListClient().put(this.cardAccountNumber, this.cardCreditName);
+        client.getListOfAccounts().put(this.cardAccountNumber, this.cardCreditName);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat();
+        System.out.println(client.getName() + " вам одобрен " + this.cardCreditName +
+                "\n________________ИНФОРМАЦИЯ__________________" +
+                "\nномер счёта: " + cardAccountNumber +
+                "\nдата открытия: " + dateFormat.format(cardOpeningDate) +
+                "\nпроцентная ставка: " + this.cardPtc +
+                "\nсумма займа: " + cardAmount +
+                "\n____________________________________________");
+    }
+
 }
